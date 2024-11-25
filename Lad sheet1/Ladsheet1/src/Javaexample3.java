70b4a53b3a75a917a8051dbf94d402165c7be939
Budsarakam <singhnathbusrakham@gmail.com>
2024-11-25 14:14:41 +0700

    LadSheet1_SecondCommit

--- Lad sheet1/Ladsheet1/src/Javaexample3.java
+++ Lad sheet1/Ladsheet1/src/Javaexample3.java
@@ -1,4 +1,13 @@
-import 
+import javax.swing.JOptionPane;
 public class Javaexample3 {
 
+	public static void main(String[] args) {
+		String input =
+				JOptionPane.showInptDialog(null, "Input Time Parking (minute):");
+		if(input!= null &&!input.isEmpty()) {
+			try {
+				int minutes
+			}
+		}
+	}
 }
