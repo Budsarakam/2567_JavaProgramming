49e16e7c419b2d0ee7174b304ea4b1edb89fcb22
Budsarakam <singhnathbusrakham@gmail.com>
2025-02-24 14:19:12 +0700

    Practice

--- LadSheet11_FileOperation/src/Lad1103.java
+++ LadSheet11_FileOperation/src/Lad1103.java
@@ -1,4 +1,45 @@
+import java.util.*;
 
 public class Lad1103 {
+	private String accountNumber;
+	private String password;
+	
+	public ATMData (String accountNumber, String password) {
+		this.accountNumber = accountNumber;
+		this.password = password;
+	}
+	
+	public String getID() {
+		return accountNumber;
+	}
+	
+	public String getPass() {
+		return password;
+	}
+}
 
+class ATMChecking extends ATMData {
+	private int money;
+	
+	public ATMChecking(String accountNumber, String password, int money) {
+		super(accountNumber, password);
+		this.money = money;
+	}
+	
+	public boolean checkBookBank() {
+		return true;
+	}
+	
+	public void show() {
+		System.out.println("Account: " + getID());
+		System.out.println("Avaliable Balance: " + money);
+	}
+}
+
+public class ATMBanking {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		
+	}
 }
